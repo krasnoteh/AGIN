07199baf9f8ccf9aa9b070aa985e17ead64ef86f
user <krasnoteh@gmail.com>
2026-02-02 09:56:55 +0300

    updated server script

--- scripts/run_websocket_server.py
+++ scripts/run_websocket_server.py
@@ -5,20 +5,23 @@ import uvicorn
 from agin.servers.websocket_server.router import router, get_stream_processor_service
 from agin.servers.websocket_server.stream_processor_service import StreamProcessorService
 
-app = FastAPI()
+def main():
+    app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
-stream_processor_service = StreamProcessorService(config_path="configs/stream_processor_config.json")
-app.dependency_overrides[get_stream_processor_service] = lambda: stream_processor_service
-app.include_router(router, prefix="/services/agin")
+    stream_processor_service = StreamProcessorService(config_path="configs/stream_processor_config.json")
+    app.dependency_overrides[get_stream_processor_service] = lambda: stream_processor_service
+    app.include_router(router, prefix="/services/agin")
+
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
 
 
-
-uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    main()
